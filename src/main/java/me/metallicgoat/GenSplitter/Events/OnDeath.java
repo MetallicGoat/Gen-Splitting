4c3668193b8897e0a9a84d392fe27294c5cad1b9
MetallicGoat <the.great.metallic.goat@gmail.com>
2021-09-09 17:04:59 -0400

    a lot i think. i dont remember

--- src/main/java/me/metallicgoat/GenSplitter/Events/OnDeath.java
+++ src/main/java/me/metallicgoat/GenSplitter/Events/OnDeath.java
@@ -4,6 +4,7 @@ import de.marcely.bedwars.api.BedwarsAPI;
 import de.marcely.bedwars.api.arena.Arena;
 import de.marcely.bedwars.api.event.player.PlayerDeathInventoryDropEvent;
 import de.marcely.bedwars.api.event.player.PlayerDeathInventoryDropEvent.Handler;
+
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -20,10 +21,9 @@ import org.bukkit.plugin.Plugin;
 public class OnDeath implements Listener {
     @EventHandler
     public void deathItemDrop(PlayerDeathInventoryDropEvent e) {
-        Main plugin = Main.getInstance();
         e.getHandlerQueue().clear();
         Player killer = e.getPlayer().getKiller();
-        if (plugin.getConfig().getBoolean("Auto-Collect.Enabled")) {
+        if (plugin().getConfig().getBoolean("Auto-Collect.Enabled")) {
             if (killer != null && killer.getGameMode() != GameMode.SPECTATOR) {
                 e.addHandlerToTop(Handler.DEFAULT_AUTO_PICKUP);
                 e.addHandlerToTop(Handler.DEFAULT_KEEP_SPAWNERS);
@@ -36,19 +36,54 @@ public class OnDeath implements Listener {
         }
     }
 
+    /*
+    public void execute(Player player, Arena arena, Player killer, List<ItemStack> droppedItems, AtomicInteger droppedExp) {
+                if(killer == null || droppedItems.isEmpty())
+                    return;
+
+                double percentageKept = 0.01 * plugin().getConfig().getInt("Auto-Collect.Percentage-Kept");
+
+                droppedItems.forEach(itemStack -> {
+                    int amountToGive = (int) (itemStack.getAmount() * percentageKept);
+                    itemStack.setAmount(amountToGive);
+                });
+
+
+                final Collection<ItemStack> remaining =
+                        killer.getInventory().addItem(droppedItems.toArray(new ItemStack[0])).values();
+                final int startCount = droppedItems.size();
+
+                droppedItems.clear();
+                droppedItems.addAll(remaining);
+
+                if(startCount - remaining.size() >= 1)
+                    BedwarsAPILayer.INSTANCE.playSound(killer, "PICKUP_ITEM");
+            }
+        };
+     */
+
+
+
     public PlayerDeathInventoryDropEvent.Handler itemDrop() {
         return new Handler() {
             public Plugin getPlugin() {
-                return Main.getInstance();
+                return plugin();
             }
 
             @Override
             public void execute(Player player, Arena arena, Player player1, List<ItemStack> list, AtomicInteger atomicInteger) {
-                Main plugin = Main.getInstance();
-                for(String itemName : plugin.getDropMaterials()){
+
+                double percentageKept = 0.01 * plugin().getConfig().getInt("Auto-Collect.Percentage-Kept");
+
+                list.forEach(itemStack -> {
+                    int amountToGive = (int) Math.ceil(itemStack.getAmount() * percentageKept);
+                    itemStack.setAmount(amountToGive);
+                });
+
+                for(String itemName : plugin().getDropMaterials()){
                     String name = getName(list, itemName);
-                    if(name != null) {
-                        String message = plugin.getConfig().getString("Auto-Collect.Message");
+                    if(name != null && player1 != null) {
+                        String message = plugin().getConfig().getString("Auto-Collect.Message");
                         String messageFormatted = message
                                 .replace("%amount%", Integer.toString(getAmount(list, itemName)))
                                 .replace("%item%", name);
@@ -70,11 +105,15 @@ public class OnDeath implements Listener {
                 for (ItemStack item : list) {
                     if (item != null && itemName.contains(item.getType().name())) {
                         DropType drop = BedwarsAPI.getGameAPI().getDropTypeByDrop(item);
+                        assert drop != null;
                         return drop.getName();
                     }
                 }
                 return null;
             }
         };
+    }
+    private static Main plugin(){
+        return Main.getInstance();
     }
 }
