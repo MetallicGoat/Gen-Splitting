2c6afedae9dea81da4b55596798fd07db13ddbe6
Marcel <8513034+MrEAlderson@users.noreply.github.com>
2024-08-31 12:40:32 +0200

    Removing lore is no longer needed

--- src/main/java/me/metallicgoat/gensplitter/GenSplitterPlugin.java
+++ src/main/java/me/metallicgoat/gensplitter/GenSplitterPlugin.java
@@ -13,8 +13,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class GenSplitterPlugin extends JavaPlugin {
 
-  private static final byte MBEDWARS_API_NUM = 100;
-  private static final String MBEDWARS_API_NAME = "5.4";
+  private static final byte MBEDWARS_API_NUM = 111;
+  private static final String MBEDWARS_API_NAME = "5.4.12";
   private static GenSplitterPlugin instance;
   private final Server server = getServer();
   private GenSplitterAddon addon;
